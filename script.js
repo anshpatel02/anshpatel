d9630b3367301b53ae531e7757bbd0200190a507
anshpatel02 <74204120+anshpatel02@users.noreply.github.com>
2020-11-10 10:22:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  
+    
+    window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
     /* Animation on Scroll */
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
